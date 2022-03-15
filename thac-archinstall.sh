24b42d4f5b83465744bc68537438edb7b3e08cff
thacxd <101656155+thacxd@users.noreply.github.com>
2022-03-15 20:41:03 +0200

    Update thac-archinstall.sh

--- thac-archinstall.sh
+++ thac-archinstall.sh
@@ -2,7 +2,7 @@
 read -p "disk " diskvar &&
 cfdisk $diskvar
 read -p "root partition " fulldiskvar &&
-mkfs.ext4 $fulldiskvar &&
+mkfs.ext4 -F $fulldiskvar &&
 mount $fulldiskvar /mnt &&
 pacstrap /mnt base base-devel linux-zen linux-firmware linux-headers vim grub dhcpcd &&
 genfstab -U /mnt >> /mnt/etc/fstab &&
@@ -10,5 +10,6 @@ read -p "username: " uservar &&
 arch-chroot /mnt useradd -mg wheel $uservar &&
 arch-chroot /mnt passwd $uservar &&
 arch-chroot /mnt grub-install $diskvar &&
-./arch-chroot.sh
+chmod +x arch-chroot.sh &&
+./arch-chroot.sh &&
 echo "Install Finished"

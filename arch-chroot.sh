92def4b80fc5262b8743bd714ac19870fc98e0e9
thacxd <101656155+thacxd@users.noreply.github.com>
2022-03-16 01:59:55 +0200

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -4,12 +4,11 @@ echo -e "en_US.UTF-8 UTF-8\nen_US ISO-8859-1" > /etc/locale.gen &&
 locale-gen &&
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf &&
 echo "KEYMAP=fi" >> /etc/vconsole.conf &&
-timedatectl set-timezone Europe/Helsinki &&
-timedatectl set-ntp true &&
+ln -sf /usr/share/zoneinfo/Europe/Helsinki /etc/localtime &&
 grub-mkconfig -o /boot/grub/grub.cfg &&
 systemctl enable dhcpcd &&
 echo -e "--save /etc/pacman.d/mirrorlist\n--country Finland,Sweden,\n--protocol https\n--latest 5" > /etc/xdg/reflector/reflector.conf &&
-systemctl start reflector
+systemctl enable reflector
 #pacman -S --noconfirm xorg pipewire pipewire-pulse pipewire-alsa pipewire-jack xfce4 xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin pavucontrol feh mpv firefox gvfs xarchiver unzip git wget xfce4-screenshooter noto-fonts-cjk noto-fonts-emoji noto-fonts sddm &&
 #systemctl enable sddm
 clear &&

8635c702d5276f316f04de145fa3e39dfde62abe
thacxd <101656155+thacxd@users.noreply.github.com>
2022-03-15 20:44:46 +0200

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -2,7 +2,7 @@
 echo -e "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/99_wheel &&
 echo -e "en_US.UTF-8 UTF-8\nen_US ISO-8859-1" > /etc/locale.gen &&
 locale-gen &&
-echo "LANG=en_US.UTF-8" >> /etc/locale.conf %%
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf &&
 echo "KEYMAP=fi" >> /etc/vconsole.conf &&
 ln -sf /usr/share/zoneinfo/Europe/Helsinki /etc/localtime &&
 grub-mkconfig -o /boot/grub/grub.cfg &&

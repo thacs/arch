49da2d0ae5e21c017fd5b9845305e5fb9924c5d7
thacxd <101656155+thacxd@users.noreply.github.com>
2022-03-17 22:18:46 +0200

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -4,11 +4,8 @@ echo -e "en_US.UTF-8 UTF-8\nen_US ISO-8859-1" > /etc/locale.gen &&
 locale-gen &&
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf &&
 echo "KEYMAP=fi" >> /etc/vconsole.conf &&
-ln -sf /usr/share/zoneinfo/Europe/Helsinki /etc/localtime &&
 grub-mkconfig -o /boot/grub/grub.cfg &&
 systemctl enable dhcpcd &&
-echo -e "--save /etc/pacman.d/mirrorlist\n--country Finland,Sweden,\n--protocol https\n--latest 5" > /etc/xdg/reflector/reflector.conf &&
-systemctl enable reflector &&
 clear &&
 read -p "username: " uservar &&
 useradd -mg wheel $uservar &&

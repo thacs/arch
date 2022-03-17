5441f2eb5bdd5944759290e77041702caa418b3b
thacxd <101656155+thacxd@users.noreply.github.com>
2022-03-17 20:43:29 +0200

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -9,8 +9,6 @@ grub-mkconfig -o /boot/grub/grub.cfg &&
 systemctl enable dhcpcd &&
 echo -e "--save /etc/pacman.d/mirrorlist\n--country Finland,Sweden,\n--protocol https\n--latest 5" > /etc/xdg/reflector/reflector.conf &&
 systemctl enable reflector
-pacman -S --noconfirm xorg pipewire pipewire-pulse pipewire-alsa pipewire-jack xfce4 xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin pavucontrol feh mpv firefox gvfs xarchiver unzip git wget xfce4-screenshooter noto-fonts-cjk noto-fonts-emoji noto-fonts sddm &&
-systemctl enable sddm
 clear &&
 read -p "username: " uservar &&
 useradd -mg wheel $uservar &&

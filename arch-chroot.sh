38d4f97403a2b1b528ee332002a31c6f68c6cab5
thac <101656155+thacs@users.noreply.github.com>
2022-04-09 22:24:02 +0300

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -8,8 +8,6 @@ grub-mkconfig -o /boot/grub/grub.cfg &&
 systemctl enable dhcpcd &&
 ln -sf /usr/share/zoneinfo/Europe/Helsinki /etc/localtime &&
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf &&
-pacman -Sy --noconfirm xorg pipewire pipewire-pulse pipewire-alsa pipewire-jack xfce4 xfce4-goodies sddm feh mpv firefox gvfs xarchiver unzip git wget noto-fonts-cjk noto-fonts-emoji noto-fonts helvum qbittorrent audacity gnome-calculator gnome-keyring discord gimp obs-studio steam reflector p7zip &&
-systemctl enable sddm &&
 clear &&
 read -p "username: " uservar &&
 useradd -mg wheel $uservar &&

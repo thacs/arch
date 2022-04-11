8784a849f48dcc99257ab57b30e52f56db58519a
thac <101656155+thacs@users.noreply.github.com>
2022-04-11 16:03:16 +0300

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -5,9 +5,14 @@ locale-gen &&
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf &&
 echo "KEYMAP=fi" >> /etc/vconsole.conf &&
 grub-mkconfig -o /boot/grub/grub.cfg &&
-systemctl enable dhcpcd &&
 ln -sf /usr/share/zoneinfo/Europe/Helsinki /etc/localtime &&
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf &&
+echo "arch" >> /etc/hostname &&
+pacman -Sy --noconfirm networkmanager xorg pipewire pipewire-pulse pipewire-alsa pipewire-jack xterm reflector
+systemctl enable NetworkManager &&
+echo -e 'Section "InputClass"\nIdentifier "keyboard-all"\nDriver "evdev"\nMatchIsKeyboard "on"\nOption "XkbLayout" "fi"\nOption "XkbModel" "pc105"\nEndSection' >> /etc/X11/xorg.conf.d/00-keyboard.conf &&
+echo -e "--save /etc/pacman.d/mirrorlist\n--country Finland,Sweden,\n--protocol https\n--latest 5" >> /etc/xdg/reflector/reflector.conf &&
+systemctl enable reflector &&
 clear &&
 read -p "username: " uservar &&
 useradd -mg wheel $uservar &&

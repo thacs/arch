21d4c83d9e014921faee1e848c15dca8ae3bef49
thac <101656155+thacs@users.noreply.github.com>
2022-04-12 15:44:33 +0300

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+clear &&
+read -p "username: " uservar &&
+useradd -mg wheel $uservar &&
+clear &&
+passwd $uservar &&
+clear
 echo -e "%wheel ALL=(ALL) ALL" > /etc/sudoers.d/99_wheel &&
 echo -e "en_US.UTF-8 UTF-8\nen_US ISO-8859-1" > /etc/locale.gen &&
 locale-gen &&
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf &&
 echo "KEYMAP=fi" >> /etc/vconsole.conf &&
-grub-mkconfig -o /boot/grub/grub.cfg &&
 ln -sf /usr/share/zoneinfo/Europe/Helsinki /etc/localtime &&
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf &&
 echo "arch" >> /etc/hostname &&
@@ -15,9 +20,4 @@ echo -e 'Section "InputClass"\n	Identifier "keyboard-all"\n	Driver "evdev"\n	Mat
 echo -e "--save /etc/pacman.d/mirrorlist\n--country Finland,Sweden,\n--protocol https\n--latest 5" >> /etc/xdg/reflector/reflector.conf &&
 echo -e 'Section "InputClass"\n    Identifier "My Mouse"\n    Driver "libinput"\n    MatchIsPointer "yes"\n    Option "AccelProfile" "flat"\n    Option "AccelSpeed" "0"\nEndSection' >> /etc/X11/xorg.conf.d/50-mouse-acceleration.conf &&
 systemctl enable reflector &&
-clear &&
-read -p "username: " uservar &&
-useradd -mg wheel $uservar &&
-clear &&
-passwd $uservar &&
 clear

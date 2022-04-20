6b61a8d02ccc9834cd4726ea2e51bf041f4db1e4
thac <101656155+thacs@users.noreply.github.com>
2022-04-20 19:23:33 +0300

    Update afterinstall.sh

--- afterinstall.sh
+++ afterinstall.sh
@@ -6,4 +6,5 @@ sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf &&
 pacman -Sy --noconfirm xorg xorg-xinit xterm reflector noto-fonts-cjk noto-fonts-emoji noto-fonts pipewire wireplumber pipewire-pulse pipewire-alsa pipewire-jack pavucontrol firefox &&
 echo -e 'Section "InputClass"\n	Identifier "keyboard-all"\n	Driver "evdev"\n	MatchIsKeyboard "on"\n	Option "XkbLayout" "fi"\nEndSection' >> /etc/X11/xorg.conf.d/00-keyboard.conf &&
 echo -e 'Section "InputClass"\n    Identifier "My Mouse"\n    Driver "libinput"\n    MatchIsPointer "yes"\n    Option "AccelProfile" "flat"\n    Option "AccelSpeed" "0"\nEndSection' >> /etc/X11/xorg.conf.d/50-mouse-acceleration.conf &&
-
+clear &&
+echo "Finished"

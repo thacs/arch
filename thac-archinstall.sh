5527d9e77c3b897dba029d56e4ed940eaced5124
thac <101656155+thacs@users.noreply.github.com>
2022-04-20 19:57:50 +0300

    Update thac-archinstall.sh

--- thac-archinstall.sh
+++ thac-archinstall.sh
@@ -9,12 +9,11 @@ read -p "root partition " fulldiskvar &&
 clear &&
 mkfs.ext4 -F $fulldiskvar &&
 mount $fulldiskvar /mnt &&
-pacstrap /mnt base base-devel linux linux-firmware linux-headers vim grub networkmanager &&
+pacstrap /mnt base base-devel linux linux-firmware linux-headers vim grub &&
 sleep 1 &&
 genfstab -U /mnt >> /mnt/etc/fstab &&
 arch-chroot /mnt grub-install $diskvar &&
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg &&
-cp afterinstall.sh /mnt &&
 cp arch-chroot.sh /mnt &&
 cd /mnt &&
 chmod +x arch-chroot.sh &&

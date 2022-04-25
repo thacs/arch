9c5cfe084e5c7cc8e5c5c3d34de3df9832fce8aa
thac <101656155+thacs@users.noreply.github.com>
2022-04-25 05:32:11 +0300

    Update arch-chroot.sh

--- arch-chroot.sh
+++ arch-chroot.sh
@@ -12,10 +12,11 @@ echo "LANG=en_US.UTF-8" >> /etc/locale.conf &&
 echo "KEYMAP=fi" >> /etc/vconsole.conf &&
 echo "arch" >> /etc/hostname &&
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf &&
-pacman -Sy --noconfirm networkmanager xorg xorg-xinit xterm reflector noto-fonts-cjk noto-fonts-emoji noto-fonts pipewire wireplumber pipewire-pulse pipewire-alsa pipewire-jack pavucontrol firefox git &&
+pacman -Sy --noconfirm networkmanager reflector xorg pipewire pipewire-pulse pipewire-alsa pipewire-jack xterm &&
 echo -e 'Section "InputClass"\n	Identifier "keyboard-all"\n	Driver "evdev"\n	MatchIsKeyboard "on"\n	Option "XkbLayout" "fi"\nEndSection' >> /etc/X11/xorg.conf.d/00-keyboard.conf &&
 echo -e 'Section "InputClass"\n    Identifier "My Mouse"\n    Driver "libinput"\n    MatchIsPointer "yes"\n    Option "AccelProfile" "flat"\n    Option "AccelSpeed" "0"\nEndSection' >> /etc/X11/xorg.conf.d/50-mouse-acceleration.conf &&
 echo -e "--save /etc/pacman.d/mirrorlist\n--country Finland,Sweden,\n--protocol https\n--latest 5" >> /etc/xdg/reflector/reflector.conf &&
+grub-mkconfig -o /boot/grub/grub.cfg &&
 ln -sf /usr/share/zoneinfo/Europe/Helsinki /etc/localtime
 systemctl enable reflector &&
 systemctl enable NetworkManager &&
